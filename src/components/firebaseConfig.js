3a1e03ca8cd3b6c698e88753c3cffe366559ba7a
dburdyga <dariaburdyga@gmail.com>
2019-04-21 12:57:32 +0300

    Initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyB3rMT17Frj8nWZqtj7AcWNkFPmJ_U4v74",
+  authDomain: "empmanager-762a3.firebaseapp.com",
+  databaseURL: "https://empmanager-762a3.firebaseio.com",
+  projectId: "empmanager-762a3",
+  storageBucket: "empmanager-762a3.appspot.com",
+  messagingSenderId: "419256166129"
+};
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import ViewEmployee from '@/components/ViewEmployee';
+import NewEmployee from '@/components/NewEmployee';
+import EditEmployee from '@/components/EditEmployee';
+import Login from '@/components/Login';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee
+    },
+    {
+      path: '/:employee_id',
+      name: 'view-employee',
+      component: ViewEmployee
+    },
+    {
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
+      component: EditEmployee
+    }
+  ]
+})
